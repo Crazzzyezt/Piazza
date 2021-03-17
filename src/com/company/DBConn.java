e00ea397024060cb73a873a3b99213d9933b929d
Edson Gammelsæter Reistad <edsongr@stud.ntnu.no>
2021-03-17 20:59:49 +0100

    added execute()

--- src/com/company/DBConn.java
+++ src/com/company/DBConn.java
@@ -24,11 +24,12 @@ public class DBConn {
             String url = "jdbc:mysql://mysql.stud.ntnu.no:3306/edsongr_eistad_datab?autoReconnect=true&useSSL=false";
             conn = DriverManager.getConnection(url, "edsongr_eistad", "passord");
             System.out.println("2");
-            PreparedStatement statement=conn.prepareStatement("INSERT INTO CourseID VALUES ( (?), (?), (?) ) ");
+            PreparedStatement statement=conn.prepareStatement("INSERT INTO Course VALUES ( (?), (?), (?) ) ");
             System.out.println("3");
             statement.setInt(1, 1);
             statement.setString(2, "Ledelse");
             statement.setString(3, "Spring");
+            statement.execute();
         }
         catch(Exception e) {
             e.printStackTrace();
