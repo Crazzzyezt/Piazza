fbb749bca2fdb1e3ce6402a9ab98b7b6b22f71a8
Edson Gammelsæter Reistad <edsongr@stud.ntnu.no>
2021-03-19 13:33:01 +0100

    implemented passwordChecks

--- src/com/company/DBConn.java
+++ src/com/company/DBConn.java
@@ -1,8 +1,6 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.Properties;
 
 
@@ -16,19 +14,45 @@ public class DBConn {
     public void connect() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("0");
+            //System.out.println("0");
             java.util.Properties p = new Properties();
             p.put("user", "edsongr_eistad");
             p.put("password", "passord");
-            System.out.println("1");
+            //System.out.println("1");
             String url = "jdbc:mysql://mysql.stud.ntnu.no:3306/edsongr_eistad_datab?autoReconnect=true&useSSL=false";
             conn = DriverManager.getConnection(url, "edsongr_eistad", "passord");
-            System.out.println("2");
-            PreparedStatement statement=conn.prepareStatement("INSERT INTO Course VALUES ( (?), (?), (?) ) ");
-            System.out.println("3");
-            statement.setInt(1, 1);
-            statement.setString(2, "Ledelse");
-            statement.setString(3, "Spring");
+            System.out.println("successfully established connection");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Boolean checkPassword(String username, String password) {
+        try {
+            PreparedStatement statement = conn.prepareStatement("SELECT name from User where password=(?)");
+            statement.setString(1, password);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                if (rs.getString("name").equals(username)) {
+                    return true;
+                }
+            }
+            return false;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
+    public void insertCourse(String name, String term) {
+        try {
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO Course(CourseName, term) VALUES ( (?), (?) ) ");
+            System.out.println("inserting " + name + "into Course");
+            statement.setString(1, name);
+            statement.setString(2, term);
             statement.execute();
         }
         catch(Exception e) {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         DBConn DBconn = new DBConn();
         DBconn.connect();
+        System.out.println(DBconn.checkPassword("Arne", "arne123"));
     }
 }
